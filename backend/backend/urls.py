843fd6df3c2c6cb5194214162bf5dafae06f033f
Kanai Lomayani <k.mahenge@gmail.com>
2022-11-10 11:33:08 +0300

    Update: settings and routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from settings.views import *
+
+from posts.views import *
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'dashboard': reverse('dashboard', request=request, format=format),
+        'settings': reverse('settings', request=request, format=format),
+        'posts': reverse('posts', request=request, format=format)
+    })
+
+
+router = routers.DefaultRouter()
+router.register(r'settings', SettingViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'districts', DistrictViewSet)
+router.register(r'regions', RegionViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'post-reactions', PostReactionViewSet)
+router.register(r'comments', PostCommentViewSet)
+router.register(r'comment-reaction', PostCommentReactionViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
